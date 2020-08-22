edaf1092b26c9154ee1b36106efb61e8a1ff71e4
Douglas Matos da Silva <douglas.silva@intelie.com.br>
2020-08-22 17:14:02 -0300

    First commit

---
+++ ContaCorrente.js
@@ -0,0 +1,20 @@
+export class ContaCorrente{
+    agencia;
+     // #saldo =0 https://github.com/tc39/proposal-class-fields#private-fields
+    _saldo = 0;
+
+    sacar(valor){
+        if(this._saldo >= valor){
+            this._saldo -= valor;
+            return valor;
+        }
+    }
+
+    depositar(valor){
+        if(valor <= 0)
+        {
+            return;
+        } 
+        this._saldo += valor;           
+    }
+}
---
+++ index.js
@@ -0,0 +1,24 @@
+import { Cliente } from './Cliente,js'
+import { ContaCorrente } from './ContaCorrente.js'
+
+
+const cliente1 = new Cliente();
+cliente1.nome = "Ricardo";
+cliente1.cpf = 11122233309;
+
+const cliente2 = new Cliente();
+cliente2.nome = "Alice";
+cliente2.cpf = 88822233309;
+
+
+const contaCorrenteRicardo = new ContaCorrente();
+contaCorrenteRicardo.agencia = 1001;
+
+contaCorrenteRicardo.depositar(-100);
+contaCorrenteRicardo.depositar(100);
+contaCorrenteRicardo.depositar(100);
+
+const valorSacado = contaCorrenteRicardo.sacar(50);
+console.log(valorSacado);
+
+console.log(contaCorrenteRicardo);
